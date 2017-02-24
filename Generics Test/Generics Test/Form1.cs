548d60b3de0e313f1230af826c7f46d95e2e8860
Max Gordin <mgordin@umich.edu>
2017-02-23 19:22:49 -0500

    got everything working as i wanted

---
+++ Generics Test/Generics Test/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generics_Test
+{
+    public partial class Form1 : Form
+    {
+
+        CheckBox[] testC;
+        Label[] testL;
+
+        public Form1()
+        {
+            InitializeComponent();
+            testC = new CheckBox[] { checkBox1, checkBox2 };
+            testL = new Label[] { label1, label2 };
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changeVisibility<T> (T[] test) where T: Control
+        {
+            for(int i = 0; i< test.Length; i++)
+            {
+                test[i].Visible = !test[i].Visible;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changeVisibility(testC);
+            changeVisibility(testL);
+        }
+    }
+}
